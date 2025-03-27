f58211b2c51149057f5b8f01cc4bdfc7d940979b
Kevian.Chen <yoan.chin@qq.com>
2025-03-27 16:09:34 +0800

    upgrade to 1.51.1
    upgrade to 1.51.1

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,23 +11,24 @@ export default defineConfig({
   testDir,
   timeout: 120_000,
   expect: { timeout: 10_000 },
-  reporter: [    [
-    "./src/index.ts",
-    {
-      jira: {
-        url: "https://client.atlassian.net/",
-        type: "cloud", // cloud, server
-        apiVersion: "1.0",
-      },
-      cloud: {
-        client_id: "B013993C259B4E528537FC5BAB94B44D",
-        client_secret: "6e7f761f4d67aa2e3e0c9fa77a6315b7d5ee682da8f8576563aad3bb680ec971",
-      },
-      testExecution: 'MYT-4',
-      uploadScreenShot: true,
-      uploadTrace: true,
-    },
-  ],
+  reporter: [  
+  // [
+  //   "./src/index.ts",
+  //   {
+  //     jira: {
+  //       url: "https://client.atlassian.net/",
+  //       type: "cloud", // cloud, server
+  //       apiVersion: "1.0",
+  //     },
+  //     cloud: {
+  //       client_id: "B013993C259B4E528537FC5BAB94B44D",
+  //       client_secret: "6e7f761f4d67aa2e3e0c9fa77a6315b7d5ee682da8f8576563aad3bb680ec971",
+  //     },
+  //     testExecution: 'MYT-4',
+  //     uploadScreenShot: true,
+  //     uploadTrace: true,
+  //   },
+  // ],
     cucumberReporter('html', {
       outputFile: 'cucumber-report/index.html',
       externalAttachments: true,
